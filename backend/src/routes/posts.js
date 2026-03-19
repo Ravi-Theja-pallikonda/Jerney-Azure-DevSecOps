1ce639864329abaef3386a2d6a3de9a30956b37e
Ravi-Theja-pallikonda <ravitheja.pallikonda@gmail.com>
2026-03-19 14:57:12 +0530

    added backend code

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { pool } = require('../db');
+const { pool } = require('./db');
 
 // GET all posts (newest first)
 router.get('/', async (req, res) => {
